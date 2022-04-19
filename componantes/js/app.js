2802d08420995f1898267bdcabbebcda209c648d
Afridi <afridi.hq97@gmail.com>
2022-04-19 23:58:16 +0530

    first commit

---
+++ componantes/js/app.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById("button");
+btn.addEventListener('click',function () {
+    alert("Thanks for your Submission");
+});
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
